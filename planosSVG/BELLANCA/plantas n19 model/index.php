adf8bdebb89eb194da324a816954d42116904a5c
zwebmaster3 <zoemrccavl93@gmail.com>
2017-09-11 13:00:00 -0500

    subida de arcvivos

---
+++ planosSVG/BELLANCA/plantas n19 model/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="UTF-8">
+	<style type="text/css">
+		svg{
+			position: absolute;		
+			width: 100%;
+			height: 100%;
+		}
+		img{
+			position: absolute;
+			z-index: -1;
+		}
+		#container{
+			margin: 0 0;
+			padding: 0 0;
+		}
+
+		
+	</style>
+	<style>.cls-1{fill:none;stroke:#B30000;stroke-miterlimit:10;}</style>
+</head>
+<body>​​
+	<svg>
+		<polygon class="cls-1" points="520.09 313.51 520.09 572.17 261.26 572.17 261.26 331.26 349.1 331.26 349.1 313.35 520.09 313.51"/>
+		<polygon class="cls-1" points="521.28 0.57 521.28 313.35 349.1 313.35 349.1 294.26 261.02 294.26 261.02 0.5 521.28 0.57"/>
+		<polygon class="cls-1" points="0.5 0.5 0.5 237.62 109.26 237.62 109.26 294.38 261.02 294.38 261.02 0.5 0.5 0.5"/>
+		<polygon class="cls-1" points="1.26 571.76 260.57 571.76 260.57 331.82 108.76 331.82 108.76 387.26 1.26 387.26 1.26 571.76"/>
+		<polygon class="cls-1" points="1.87 718.45 520.09 718.45 520.09 0.57 0.5 0.57 1.87 718.45"/>
+	</svg>
+		
+	<img id="myimg" src="PLANTAS N19-Model.jpg">
+	<script type="text/javascript">
+// 		var i = document.getElementById('myimg');
+// ​
+// 		var i2 = new Image();
+// 		i2.onload = function() {		  
+// 		  var svg = document.getElementsByTagName('svg');
+// 		};
+// ​
+// 		document.getElementsByTagName('svg').style.width = i.src;
+// 		document.getElementsByTagName('svg').style.height = i.height;
+	</script>
+​
+</body>
+</html>
