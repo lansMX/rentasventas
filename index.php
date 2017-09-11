c6a9f42e13d77521024b82dd0ca036ed5bb7ee00
zwebmaster3 <zoemrccavl93@gmail.com>
2017-09-11 17:59:03 -0500

    add tooltip

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 <html>
 <head>
 	<title>test</title>
-	<meta charset="utf-8"></meta>
+	<meta charset="utf-8">
 	<link id="estilossvg" rel="stylesheet" href="css/local.css">
-	
+	<link rel="stylesheet" type="text/css" href="bower_components/tooltipster/dist/css/tooltipster.bundle.min.css"/>
+  <!-- <link rel="stylesheet" type="text/css" href="bower_components/tooltipster/dist/css/tooltipster-sideTip-punk.min.css"/> -->
+
 	<style type="text/css">
 		body{
 			background-color: #fff;
@@ -30,7 +32,7 @@
 			height: 490px;
 		}
 
-		.calculador{			
+		.calculador{
 			float: right;
 		}
 
@@ -66,10 +68,10 @@
 		}
 
 	</style>
-	
+
 
 </head>
-<body>	
+<body>
 	<div class="content">
 		<div>
 			<select name="SP" id="SP" class="" tabindex="1" style="height:43px; border:1px solid #FFF; font-size:33px; ">
@@ -79,7 +81,7 @@
 				<option value="3N">3N</option>
 			</select>
 			<hr>
-		</div>	
+		</div>
 		<div class="map">
 			<div class="mapp"></div>
 		</div>
@@ -162,8 +164,39 @@
 			</form>
 		</div>
 	</div>
+	<svg width="100%" viewBox="0 0 768 447" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 3.8.3 (29802) - http://www.bohemiancoding.com/sketch -->
+    <defs>
+        <rect id="path-1" x="0" y="0" width="768" height="447"></rect>
+        <mask id="mask-2" maskContentUnits="userSpaceOnUse" maskUnits="objectBoundingBox" x="0" y="0" width="768" height="447" fill="white">
+            <use xlink:href="#path-1"></use>
+        </mask>
+    </defs>
+    <g id="journocode" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="example">
+            <use id="Rectangle-1" stroke="#50E3C2" mask="url(#mask-2)" stroke-width="42" fill="#4A4A4A" xlink:href="#path-1"></use>
+
+						<!-- add class and title here! -->
+            <circle id="Oval-1" fill="#FF7979" cx="142.5" cy="147.5" r="92.5" class="tooltip" title="I am a circle!"></circle>
+            <polygon id="Star-1" fill="#FFF681" points="384 349 328.748186 378.047597 339.300344 316.523799 294.600687 272.952403 356.374093 263.976201 384 208 411.625907 263.976201 473.399313 272.952403 428.699656 316.523799 439.251814 378.047597" class="tooltip" title="<strong>Stars for the win</strong><br>I am a star!"></polygon>
+            <polygon id="Triangle-1" fill="#A9FFEC" points="619 72 717 223 521 223" class="tooltip" title="<p style='color:#F85D00'>Hell yeah!</p>"></polygon>
+        </g>
+    </g>
+</svg>
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script type="text/javascript" src="js/jquery.number.js"></script>
+	<script type="text/javascript" src="bower_components/tooltipster/dist/js/tooltipster.bundle.min.js"></script>
+
+  <script>
+    $(document).ready(function() {
+        $('.tooltip').tooltipster({
+        // theme: 'tooltipster-punk',
+        contentAsHTML: true
+        });
+    });
+	</script>
+
 	<script type="text/javascript">
 
 		$(document).ready(function(){
@@ -177,8 +210,13 @@
 			$("#preciototal").number(true, 2);
 			$("#preciocontado").number(true, 2);
 			$("#total").number(true, 2);
-			$("#preciomensual").number(true, 2);			
+			$("#preciomensual").number(true, 2);
+
+
 		});
+
+
+
 
 		$(".land").mouseover(function(){
 			var title = $(this).attr('title');
@@ -208,7 +246,7 @@
 			var title = $(this).attr("title");
 			$("#localSelected").text(title);
 
-			$.getJSON( "datos/infedo.json", function( data ) {
+			$.getJSON("datos/infedo.json", function( data ) {
 	          var items = [];
 	          var opciones = "";
 
@@ -218,13 +256,13 @@
 						//console.log(data.data[i].superficie);
 						$("#m2finales").val(data.data[i].superficie);
 
-						var value = $("#m2finales").val();						
-						var precio = $("#precioxm2").val();										
-				    	var total1 = value * precio;			    	
-				    	
-				    	$("#preciototal").val(total1);				    	
+						var value = $("#m2finales").val();
+						var precio = $("#precioxm2").val();
+				    	var total1 = value * precio;
+
+				    	$("#preciototal").val(total1);
 				    	$("#preciocontado").val(total1);
-				    	
+
 						$('#porcentaje').change();
 					}
           		}
@@ -263,15 +301,15 @@
 					$("#localSelected").text("México");
 					getAllSuperficie();
 					break;
-				case 2:	
+				case 2:
 					$("#svglocal").show();
 					$("#svgplaza").hide();
 					$("#estilossvg").attr("href", "css/local.css");
 					break;
 				case 3:
-					break;	
+					break;
 			}
-			
+
 		});
 
 		$("#svgplaza g").click(function(){
@@ -300,23 +338,23 @@
 
 		var asipararapido = "";
 		var urlFondo = "";
-        $.getJSON( "datos/catalogo.json", function( data ) {
-          var items = [];
-          var opciones = "";
-          
-          var idMap = "<?= $_GET['map']; ?>";
-          $.each( data.catalogo, function( key, val ) {
-          	if (val.id == idMap) {
-          		opciones = 	val.codesvg[0].n0;
-          		asipararapido = val.codesvg[0];
-          		urlFondo = val.imgfondo;
-          	}
-          	
-          });
-          // <text class='label' id='parteCuerpo' x='10' y='390'>México</text>
-          $(".mapp").append(opciones + "<img class='imgFondoPlano' src='" + urlFondo + "'>" );
+    $.getJSON( "datos/catalogo.json", function( data ) {
+      var items = [];
+      var opciones = "";
 
-        });
+      var idMap = "<?= $_GET['map']; ?>";
+      $.each( data.catalogo, function( key, val ) {
+      	if (val.id == idMap) {
+      		opciones = 	val.codesvg[0].n0;
+      		asipararapido = val.codesvg[0];
+      		urlFondo = val.imgfondo;
+      	}
+
+      });
+      // <text class='label' id='parteCuerpo' x='10' y='390'>México</text>
+      $(".mapp").append(opciones + "<img class='imgFondoPlano' src='" + urlFondo + "'>" );
+
+    });
 
 		function getAllSuperficie(){
 			$.getJSON( "datos/infedo.json", function( data ) {
@@ -327,8 +365,8 @@
 		          	console.log(i.superficie);
 		          	mtotales = mtotales + parseInt(i.superficie);
 		        });
-	          
-	          	//console.log(mtotales);	          	
+
+	          	//console.log(mtotales);
 	          	$("#m2finales").val(mtotales);
 	            var value = $("#m2finales").val();
 
@@ -358,7 +396,7 @@
      //      		}
 
 	      	});
-		}        	
+		}
       </script>
 
 </body>
